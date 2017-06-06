61527f2e3ef930e4e4787c776ee2d1501009f659
변아영 <heven24@hanmail.net>
2017-06-06 11:48:08 +0900

    Initialize

--- app/src/main/java/com/example/ayoung/frdetector/MainActivity.java
+++ app/src/main/java/com/example/ayoung/frdetector/MainActivity.java
@@ -9,40 +9,7 @@ import android.widget.Button;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-class Team implements Serializable{
-    String teamname;
-    String teamcode;
-    ArrayList<Person> persons;
-
-    public Team(){
-        super();
-    }
-    public Team(String teamname, String teamcode,Person p){
-        this.teamname = teamname;
-        this.teamcode = teamcode;
-        this.persons.add(p);
-    }
-}
-
-class Person implements Serializable{
-    boolean teamleader;
-    String name;
-    String number;
-
-    public Person(){
-        super();
-    }
-    public Person(boolean teamleader, String name, String number){
-        this.teamleader = teamleader;
-        this.name = name;
-        this.number = number;
-    }
-}
-
 public class MainActivity extends AppCompatActivity {
-
-    public static ArrayList<Team> teams = new ArrayList<Team>();
-    public static String teamcode = "0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
